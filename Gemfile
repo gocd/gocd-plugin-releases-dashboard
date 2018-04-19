000f13cd9f7aa981853fa86d7d0e9b6f8877b1b6
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2018-04-19 20:44:12 +0530

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'middleman'
+gem 'middleman-autoprefixer'
+gem 'middleman-livereload'
+gem 'middleman-gh-pages'
+gem 'octokit'
+gem 'rake'
---
+++ config.rb
@@ -0,0 +1,61 @@
+require 'active_support/core_ext/numeric/time'
+activate :relative_assets
+set :relative_links, true
+
+activate :livereload, apply_js_live: false, apply_css_live: true, no_swf: true
+
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
+
+class HealthSeverity
+  GOOD = 10
+  WARNING = 30
+  BAD = 30
+  UNKNOWN = 40
+
+  COLOR = {
+    UNKNOWN => "mdl-color--grey",
+    GOOD => "mdl-color--green",
+    WARNING => "mdl-color--amber",
+    BAD => "mdl-color--red",
+  }
+
+end
+
+
+helpers do
+  def color_for(repo)
+    HealthSeverity::COLOR[severity_for(repo) || (raise "unknown severity for repo #{repo}")]
+  end
+
+  def severity_for(repo)
+    case repo.commits_since_last_release
+    when nil
+      HealthSeverity::UNKNOWN
+    when 0
+      HealthSeverity::GOOD
+    when 1..2
+      if Time.now - repo.latest_release_date < 3.days
+        HealthSeverity::GOOD
+      elsif Time.now - repo.latest_release_date < 7.days
+        HealthSeverity::WARNING
+      else
+        30
+      end
+    when 3..5
+      HealthSeverity::WARNING
+    else
+      HEALTH_SEVERITY::BAD
+    end
+  end
+end
